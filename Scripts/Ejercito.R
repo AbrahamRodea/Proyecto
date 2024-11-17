16c44327b606c981d960b86d6b1b93915a07f86d
AbrahamRodea <irodea13@alumnos.uaq.mx>
2024-11-16 22:19:49 -0600

    Optimización 1

--- Scripts/Ejercito.R
+++ Scripts/Ejercito.R
@@ -6,25 +6,28 @@ puntos_de_partida<-c(500,1000,2000) #Asiganmos el numero de puntos necesarios pa
 
 names(puntos_de_partida)<-modos_de_juego
 
-elije_faccion<- as.numeric( readline( prompt = "Las facciones del juego son: Marines espaciales(1), Tau(2), Tiranidos(3) y Necrones(4), coloca el numero de tu faccion: "))           
-
-if( elije_faccion == 1 ){
-  puntos<-read.csv("Datos/Puntos de ejercito  - Marines espaciales.csv")
-  print( "Has elegido a los angeles de la muerte del emperador")
-  print( "Estas son las unidades disponibles: ")
-  print(paste( puntos[[1]]))
+ejercito<-function(x){
   
-} else if ( elije_faccion == 2 ){
-  puntos<-read.csv("Datos/Puntos de ejercito  - Tau.csv")
-  print( "Has elegido el bien mayor")
-  print( "Estas son las unidades disponibles: ")
-  print(paste( puntos[[1]]))
+  x<- as.numeric( readline( prompt = "Las facciones del juego son: Marines espaciales(1), Tau(2), Tiranidos(3) y Necrones(4), coloca el numero de tu faccion: "))           
   
-} else if ( elije_faccion == 4){
-  puntos<-read.csv("Datos/Puntos de ejercito  - Necron.csv")
-  print( "¿Por qué dar un nombre a la totalidad?")
-  print( "Estas son las unidades disponibles: ")
-  print(paste( puntos[[1]]))
-} 
+  if( x == 1 ){
+    puntos<-read.csv("Datos/Puntos de ejercito  - Marines espaciales.csv")
+    print( "Has elegido a los angeles de la muerte del emperador")
+    print( "Estas son las unidades disponibles: ")
+    print(paste( puntos[[1]]))
+    
+  } else if ( x == 2 ){
+    puntos<-read.csv("Datos/Puntos de ejercito  - Tau.csv")
+    print( "Has elegido el bien mayor")
+    print( "Estas son las unidades disponibles: ")
+    print(paste( puntos[[1]]))
+    
+  } else if ( x == 4){
+    puntos<-read.csv("Datos/Puntos de ejercito  - Necron.csv")
+    print( "¿Por qué dar un nombre a la totalidad?")
+    print( "Estas son las unidades disponibles: ")
+    print(paste( puntos[[1]]))
+  } 
+}
  
 
